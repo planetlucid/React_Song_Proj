88f2b8cfcd0b032488fb05a85b59503b5b472fc0
github username <github email address>
2020-06-20 19:55:58 -0400

    added style sheet

--- frontend/src/containers/Songs.js
+++ frontend/src/containers/Songs.js
@@ -1,36 +1,26 @@
-import React from 'react'
+import React from "react";
+import "./Songs.css";
 
 const Songs = (props) => (
-<div> 
+  <div>
     <h1>Songs</h1>
-    {props.songs.map(song => 
-    <div className= "SongCard">
+    {props.songs.map((song) => (
+      <div className="SongCard">
         <h1>{song.title}</h1>
-        <img src={song.img_url} alt={song.name} /> 
+        <img src={song.img_url} alt={song.name} />
         <p>artist: {song.artist}</p>
         <p>album: {song.album}</p>
         <p>genre: {song.genre}</p>
         <p>key: {song.key}</p>
-        <p>mode:  {song.mode}</p>
-        <p>sales:  {song.sales}</p>
+        <p>mode: {song.mode}</p>
+        <p>sales: {song.sales}</p>
         <p>streams: {song.streams}</p>
-        <p>bpm:  {song.bpm}</p>
-        <p>credits:  {song.credits}</p>
+        <p>bpm: {song.bpm}</p>
+        <p>credits: {song.credits}</p>
         <p>structure: {song.structure}</p>
-    </div>
-    )}
-</div>
+      </div>
+    ))}
+  </div>
 );
 
 export default Songs;
-
-
-
-
-
-
-
-
-
-
-
