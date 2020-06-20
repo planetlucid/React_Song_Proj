0cd56dd6286f8d4ac9b0d38ba1ce3b2ee3174bcc
github username <github email address>
2020-06-20 18:48:27 -0400

    react loading components successfully

---
+++ frontend/src/containers/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+ 
+// import Songs from './Songs';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        App Container
+      </div>
+    );
+  }
+}
+
+export default App;
