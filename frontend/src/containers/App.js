acc5835b569e29812435eecf0f506a06f65a7b8c
github username <github email address>
2020-06-20 19:44:36 -0400

    params manually

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -5,12 +5,32 @@ import './App.css';
 
 const songs = [
     {
-        title:"first song",
-        img_url:"the link",
+        title:"New Apartment",
+        img_url:"https://bit.ly/3djlYb4",
+        artist: "Ari Lennox",
+        album: "Shea Butter Baby",
+        genre:"R&B",
+        key:"G Minor",
+        mode: "Pentatonic",
+        sales: "700000",
+        streams:"8000000",
+        bpm: "95",
+        credits: "alot",
+        structure:"[intro, chorus, verse, bridge, outro]"
     },
     {
-        title:"second song",
-        img_url:"the link",
+        title:"How Much a Dollar Cost",
+        img_url:"https://bit.ly/2AUl1ss",
+        artist: "Kendrick Lamar",
+        album: "To Pimp A Butterfly",
+        genre:"Hip-Hop",
+        key:"D Minor",
+        mode: "Dorian",
+        sales: "1000000",
+        streams:"9000000",
+        bpm: "90",
+        credits: "alot",
+        structure:"[intro, chorus, verse, bridge, outro]"
     }
 ]
 
--- frontend/src/containers/Songs.js
+++ frontend/src/containers/Songs.js
@@ -3,8 +3,34 @@ import React from 'react'
 const Songs = (props) => (
 <div> 
     <h1>Songs</h1>
-    {props.songs.map(song => <h1>{song.name}</h1>)}
+    {props.songs.map(song => 
+    <div className= "SongCard">
+        <h1>{song.title}</h1>
+        <img src={song.img_url} alt={song.name} /> 
+        <p>artist: {song.artist}</p>
+        <p>album: {song.album}</p>
+        <p>genre: {song.genre}</p>
+        <p>key: {song.key}</p>
+        <p>mode:  {song.mode}</p>
+        <p>sales:  {song.sales}</p>
+        <p>streams: {song.streams}</p>
+        <p>bpm:  {song.bpm}</p>
+        <p>credits:  {song.credits}</p>
+        <p>structure: {song.structure}</p>
+    </div>
+    )}
 </div>
 );
 
 export default Songs;
+
+
+
+
+
+
+
+
+
+
+
