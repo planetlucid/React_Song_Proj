cbd2828414179f820be14e23a00dbb724bd82154
github username <github email address>
2020-06-21 10:38:07 -0400

    created store file and added to index

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,24 @@
-import {
-    createStore, 
-    applyMiddleware,
-    combineReducers
-  } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from "redux";
 
 //   async calls from back-end
-  import thunk from 'redux-thunk';
+import thunk from "redux-thunk";
+
+const songsReducer = (state = [], action) => {
+  switch (action.type) {
+    case "GET_SONGS_SUCCESS":
+      return action.songs;
+    default:
+      return state;
+  }
+};
+
+const reducers = combineReducers({
+songs: songsReducer
+});
+const middleware = [thunk];
+
+export default createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(...middleware)
+);
