51bae9b70534e3f8ad9af45021b6c686db9f9f0a
github username <github email address>
2020-06-21 09:52:49 -0400

    refactored into components

--- frontend/src/containers/Songs.js
+++ frontend/src/containers/Songs.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, {Component} from "react";
 import SongCard from '../components/SongCard';
 import "./Songs.css";
 
-const Songs = (props) => (
-  <div className ="SongsContainer">
-    <h1>Songs</h1>
-    {props.songs.map(song => <SongCard key={song.id}song={song} />)}
-  </div>
-);
+class Songs extends Component {
+
+    render () {
+        return (
+        <div className ="SongsContainer">
+            <h1>Songs</h1>
+            {this.props.songs.map(song => <SongCard key={song.id}song={song} />)}
+        </div>
+        );
+    }
+}
 
 export default Songs;
